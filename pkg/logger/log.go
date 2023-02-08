9161d89120d73df16fdce2f02ab438daa246b7be
vano2903 <davidevanoncini2003@gmail.com>
2023-02-08 22:45:18 +0100

    feat:add package that configure logrus

---
+++ pkg/logger/log.go
@@ -0,0 +1,30 @@
+package logger
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+func NewLogger(level, logType string) *logrus.Logger {
+	logger := logrus.New()
+
+	if logType == "text" {
+		logger.SetFormatter(&logrus.TextFormatter{})
+	} else {
+		logger.SetFormatter(&logrus.JSONFormatter{})
+	}
+
+	switch level {
+	case "debug":
+		logger.SetLevel(logrus.DebugLevel)
+	case "info":
+		logger.SetLevel(logrus.InfoLevel)
+	case "warn":
+		logger.SetLevel(logrus.WarnLevel)
+	case "error":
+		logger.SetLevel(logrus.ErrorLevel)
+	default:
+		logger.SetLevel(logrus.InfoLevel)
+	}
+
+	return logger
+}
